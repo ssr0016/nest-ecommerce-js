32376c1279a0b4231e0f5859a4df51e0610e814d
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-10 20:07:04 +0800

    compose primary key

--- src/permissions/entities/permission.entity.ts
+++ src/permissions/entities/permission.entity.ts
@@ -1,17 +1,23 @@
 import { Endpoint } from 'src/endpoint/entities/endpoint.entity';
 import { Role } from 'src/role/entities/role.entity';
-import { Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Permission {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryColumn()
+  roleName: string;
+
+  @PrimaryColumn()
+  endpointId: number;
 
   @ManyToOne(() => Role, (role) => role.permissions)
+  @JoinColumn({ name: 'roleName' })
   role: Role;
 
   @ManyToOne(() => Role, (role) => role.permissions)
+  @JoinColumn({ name: 'endpointId' })
   endpoint: Endpoint;
 
+  @Column()
   isAllow: boolean;
 }
