60cc64ede93f173a98c5f949efa1827764e63179
ssr0016 <samsonrecluta0016@gmail.com>
2025-09-16 16:55:04 +0800

    first look to upload multiple images

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -8,10 +8,11 @@ import {
   MaxFileSizeValidator,
   FileTypeValidator,
   ParseIntPipe,
+  UploadedFiles,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { API_VERSION } from 'src/_cores/constants/app.constant';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import path from 'node:path';
 import { diskStorage } from 'multer';
 
@@ -53,6 +54,30 @@ export class UploadController {
     file: Express.Multer.File,
   ) {
     await this.uploadService.upload(type, entityId, file);
+    return {
+      message: 'success',
+    };
+  }
+
+  // ['products', 'users']
+  @Post('')
+  @UseInterceptors(FilesInterceptor('files'))
+  uploadManyFile(
+    @UploadedFiles(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1048576 }),
+          new FileTypeValidator({
+            fileType: 'image/*',
+            skipMagicNumbersValidation: true,
+          }),
+        ],
+      }),
+    )
+    files: Array<Express.Multer.File>,
+  ) {
+    console.log('files', files);
+    // await this.uploadService.upload(type, entityId, files[0]);
     return {
       message: 'success',
     };
